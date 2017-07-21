a9a07328028577a672f0f2e5a07231f0f7e867b1
Michael "Tekkla" Zorn <tekkla@tekkla.de>
2017-07-21 11:01:36 +0200

    Code review
    * Adds error handling when the loaded config file does not contain an
    array.
    * Adds/Completes comments

--- Core/Framework/Amvc/App/Config.php
+++ Core/Framework/Amvc/App/Config.php
@@ -5,19 +5,20 @@ namespace Core\Framework\Amvc\App;
  * Config.php
  *
  * @author Michael "Tekkla" Zorn <tekkla@tekkla.de>
- * @copyright 2016
+ * @copyright 2016-2017
  * @license MIT
  */
 class Config
 {
-
+    
     /**
      *
      * @var array
      */
     private $config = [];
-
+    
     /**
+     * Loads a config array from a specifig file
      *
      * @param string $filename
      *
@@ -25,14 +26,23 @@ class Config
      */
     public function load(string $filename)
     {
-        if (!file_exists($filename)) {
+        // Error when the file is missing
+        if (! file_exists($filename)) {
             Throw new AppException(sprintf('Configfile "%s" does not exist.'), $filename);
         }
-
-        $this->config = include $filename;
+        
+        $config = include $filename;
+        
+        // Error when the loaded config is no array
+        if (! is_array($config)) {
+            Throw new AppException(sprintf('The loaded app config is malformed. Make sure that the loaded file "%s" returns an array.', $filename));
+        }
+        
+        $this->config = $config;
     }
-
+    
     /**
+     * Returns the config array
      *
      * @return array
      */
@@ -40,13 +50,14 @@ class Config
     {
         return $this->config;
     }
-
+    
     /**
+     * Sets/Replaces current config array
      *
      * @param array $config
      */
-    public function set(array $config) {
+    public function set(array $config)
+    {
         $this->config = $config;
     }
 }
-
